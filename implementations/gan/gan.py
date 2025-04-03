11adc10805f8e1d65c17a0083dcd09f82c205625
wellCh4n <wellCh4n@foxmail.com>
2025-04-04 02:28:29 +0800

    tomato leaf gan

--- implementations/gan/gan.py
+++ implementations/gan/gan.py
@@ -161,7 +161,7 @@ for epoch in range(opt.n_epochs):
         # Measure discriminator's ability to classify real from generated samples
         real_loss = adversarial_loss(discriminator(real_imgs), valid)
         fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
-        d_loss = (real_loss + fake_loss) / 2
+        d_loss = real_loss + fake_loss
 
         d_loss.backward()
         optimizer_D.step()
